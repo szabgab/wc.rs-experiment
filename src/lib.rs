7f06eb38022b23b3456af261386ef3e67200117a
Gabor Szabo <gabor@szabgab.com>
2024-07-08 23:35:02 +0300

    count rows

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-pub fn wc(file: &str) -> usize {
+pub fn wc(file: &str) -> (usize, usize) {
     let content = std::fs::read_to_string(file).unwrap();
-    return content.len()
+    let rows = content.split("\n").collect::<Vec<&str>>();
+    return (rows.len()-1, content.len())
 }
 
 #[cfg(test)]
@@ -10,19 +11,19 @@ mod tests {
     #[test]
     fn it_works() {
         let result = wc("files/empty.txt");
-        assert_eq!(result, 0);
+        assert_eq!(result, (0,0));
 
         let result = wc("files/hello.txt");
-        assert_eq!(result, 11);
+        assert_eq!(result, (0, 11));
 
         let result = wc("files/hello_with_newline.txt");
-        assert_eq!(result, 12);
+        assert_eq!(result, (1, 12));
 
         let result = wc("files/spaces.txt");
-        assert_eq!(result, 47);
+        assert_eq!(result, (3, 47));
 
         let result = wc("files/unicode.txt");
-        assert_eq!(result, 12);
+        assert_eq!(result, (1, 12));
 
     }
 }
