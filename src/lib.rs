6627e2bbcfa05b1605fe75a2fdfa8ded69754def
Gabor Szabo <gabor@szabgab.com>
2024-07-18 11:52:00 +0300

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[derive(PartialEq, Debug)]
 pub struct Wc {
     rows: usize,
@@ -6,18 +5,17 @@ pub struct Wc {
     bytes: usize,
 }
 
-
 pub fn wc(file: &str) -> Wc {
     let content = std::fs::read_to_string(file).unwrap();
     let rows = content.split("\n").collect::<Vec<&str>>();
     let words = content.split_whitespace().collect::<Vec<&str>>();
     let res = Wc {
-        rows: rows.len()-1,
+        rows: rows.len() - 1,
         words: words.len(),
-        bytes: content.len(),     
+        bytes: content.len(),
     };
 
-    return res
+    return res;
 }
 
 #[cfg(test)]
@@ -27,19 +25,54 @@ mod tests {
     #[test]
     fn it_works() {
         let result = wc("files/empty.txt");
-        assert_eq!(result, Wc { rows: 0, words: 0, bytes: 0} );
+        assert_eq!(
+            result,
+            Wc {
+                rows: 0,
+                words: 0,
+                bytes: 0
+            }
+        );
         assert_eq!(result.rows, 0);
 
         let result = wc("files/hello.txt");
-        assert_eq!(result, Wc { rows: 0, words: 2, bytes: 11 });
+        assert_eq!(
+            result,
+            Wc {
+                rows: 0,
+                words: 2,
+                bytes: 11
+            }
+        );
 
         let result = wc("files/hello_with_newline.txt");
-        assert_eq!(result, Wc { rows: 1, words: 2, bytes: 12});
+        assert_eq!(
+            result,
+            Wc {
+                rows: 1,
+                words: 2,
+                bytes: 12
+            }
+        );
 
         let result = wc("files/spaces.txt");
-        assert_eq!(result, Wc { rows: 3, words: 7, bytes: 47});
+        assert_eq!(
+            result,
+            Wc {
+                rows: 3,
+                words: 7,
+                bytes: 47
+            }
+        );
 
         let result = wc("files/unicode.txt");
-        assert_eq!(result, Wc { rows: 1, words: 2, bytes: 12 });
+        assert_eq!(
+            result,
+            Wc {
+                rows: 1,
+                words: 2,
+                bytes: 12
+            }
+        );
     }
 }
