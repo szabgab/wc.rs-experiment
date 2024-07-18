d3308506c6bbe9f840c5cd36ee94c3f1c5c7a308
Gabor Szabo <gabor@szabgab.com>
2024-07-18 11:51:47 +0300

    convert return value to struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
-pub fn wc(file: &str) -> (usize, usize, usize) {
+
+#[derive(PartialEq, Debug)]
+pub struct Wc {
+    rows: usize,
+    words: usize,
+    bytes: usize,
+}
+
+
+pub fn wc(file: &str) -> Wc {
     let content = std::fs::read_to_string(file).unwrap();
     let rows = content.split("\n").collect::<Vec<&str>>();
     let words = content.split_whitespace().collect::<Vec<&str>>();
-    return (rows.len()-1, words.len(), content.len())
+    let res = Wc {
+        rows: rows.len()-1,
+        words: words.len(),
+        bytes: content.len(),     
+    };
+
+    return res
 }
 
 #[cfg(test)]
@@ -12,18 +27,19 @@ mod tests {
     #[test]
     fn it_works() {
         let result = wc("files/empty.txt");
-        assert_eq!(result, (0, 0, 0));
+        assert_eq!(result, Wc { rows: 0, words: 0, bytes: 0} );
+        assert_eq!(result.rows, 0);
 
         let result = wc("files/hello.txt");
-        assert_eq!(result, (0, 2, 11));
+        assert_eq!(result, Wc { rows: 0, words: 2, bytes: 11 });
 
         let result = wc("files/hello_with_newline.txt");
-        assert_eq!(result, (1, 2, 12));
+        assert_eq!(result, Wc { rows: 1, words: 2, bytes: 12});
 
         let result = wc("files/spaces.txt");
-        assert_eq!(result, (3, 7, 47));
+        assert_eq!(result, Wc { rows: 3, words: 7, bytes: 47});
 
         let result = wc("files/unicode.txt");
-        assert_eq!(result, (1, 2, 12));
+        assert_eq!(result, Wc { rows: 1, words: 2, bytes: 12 });
     }
 }
